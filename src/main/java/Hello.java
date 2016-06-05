8de989c646c61f36295c97ec964f8563785a5b97
Sharky <sharky1414@ukr.net>
2016-06-05 18:00:39 +0300

    Initialization

---
+++ src/main/java/Hello.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Ignore on 05.06.2016.
+ */
+public class Hello {
+    public String st = "Hello";
+}
---
+++ src/test/java/Testhello.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Ignore on 05.06.2016.
+ */
+public class Testhello {
+    @Test
+    public void test1(){
+      Hello h = new Hello();
+        assert h.st.equals("Hello");
+    }
+}
