bda384f89485da63c65c2e7d6da0878b280a1dc9
Sharky <sharky1414@ukr.net>
2016-06-05 18:43:14 +0300

    Test ok

--- src/test/java/Testhello.java
+++ src/test/java/Testhello.java
@@ -12,6 +12,6 @@ public class Testhello {
     @Test
     public void test2(){
         Hello h = new Hello();
-        assert h.getStr().equals("Hello world");
+        assert h.getStr().equals("Helloworld");
     }
 }
